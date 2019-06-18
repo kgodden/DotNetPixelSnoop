56e34e1ae46fac2f86d87949d596b96f948ab94d
Kevin Godden <kgodden@ridgesolutions.ie>
2019-06-18 19:08:07 +0100

    Adding test functions, implemented IDisposable

---
+++ DotNetPixelSnoop/BmpPixelSnoop.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace DotNetPixelSnoop
+{
+    unsafe class BmpPixelSnoop : IDisposable
+    {
+        private Bitmap bmp = null;
+        private BitmapData data = null;
+        private byte* scan0 = null;
+        private int depth = 0;
+        private int lineBytes = 0;
+
+        private readonly int width;
+        private readonly int height;
+
+
+        public BmpPixelSnoop(Bitmap b)
+        {
+            bmp = b;
+            width = bmp.Width;
+            height = bmp.Height;
+
+            // Works only for: PixelFormat.Format32bppArgb
+
+            var rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            data = bmp.LockBits(rect, ImageLockMode.ReadWrite, bmp.PixelFormat);
+            depth = Bitmap.GetPixelFormatSize(data.PixelFormat) / 8; //bytes per pixel
+            scan0 = (byte*)data.Scan0.ToPointer();
+            lineBytes = bmp.Width * depth;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (bmp != null)
+                    bmp.UnlockBits(data);
+            }
+            // free native resources if there are any.
+        }
+
+        //private List<int> lineStarts;
+
+        private byte* PixelPointer(int x, int y)
+        {
+            /*
+            if (lineStarts == null)
+            {
+                lineStarts = new List<int>();
+                for (int j = 0; j != Height; j++)
+                    lineStarts.Add((int)(scan0 + j * lineBytes));
+            }*/
+
+            return scan0 + y * lineBytes + x * depth;
+            //return (byte*)lineStarts[y] + x * depth;
+
+        }
+
+        private byte* PixelPointer(int offset)
+        {
+            return scan0 + offset * depth;
+        }
+
+        public System.Drawing.Color GetPixel(int x, int y)
+        {
+            int a, r, g, b;
+
+            byte* p = PixelPointer(x, y);
+
+            b = *p++;
+            g = *p++;
+            r = *p++;
+            a = *p;
+
+            return System.Drawing.Color.FromArgb(a, r, g, b);
+        }
+
+        public struct NovaCol
+        {
+            public byte R;
+            public byte G;
+            public byte B;
+            public byte A;
+
+            public NovaCol(int r, int g, int b, int a)
+            {
+                R = (byte)r;
+                G = (byte)g;
+                B = (byte)b;
+                A = (byte)a;
+            }
+        }
+
+        public NovaCol NovaGetPixel(int x, int y)
+        {
+            int a, r, g, b;
+
+            byte* p = PixelPointer(x, y);
+
+            b = *p++;
+            g = *p++;
+            r = *p++;
+            a = *p;
+
+            return new NovaCol(a, r, g, b);
+        }
+
+
+        public System.Drawing.Color GetPixel(int pixelOffset)
+        {
+            int a, r, g, b;
+
+            byte* p = PixelPointer(pixelOffset);
+
+            b = *p++;
+            g = *p++;
+            r = *p++;
+            a = *p;
+
+            var c = new System.Drawing.Color();
+
+
+            return System.Drawing.Color.FromArgb(a, r, g, b);
+        }
+
+
+
+        public byte GetRed(int x, int y)
+        {
+            return *(PixelPointer(x, y) + 2);
+        }
+
+        public byte GetGreen(int x, int y)
+        {
+            return *(PixelPointer(x, y) + 1);
+        }
+
+        public byte GetBlue(int x, int y)
+        {
+            return *(PixelPointer(x, y));
+        }
+
+        public void SetPixel(int x, int y, System.Drawing.Color col)
+        {
+            byte* p = PixelPointer(x, y);
+
+            *p++ = col.B;
+            *p++ = col.G;
+            *p++ = col.R;
+            *p = col.A;
+        }
+
+        public int Width { get { return width; } }
+        public int Height { get { return height; } }
+
+       
+    }
+}
--- DotNetPixelSnoop/Program.cs
+++ DotNetPixelSnoop/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,285 @@ namespace DotNetPixelSnoop
     {
         static void Main(string[] args)
         {
+            TestSnoop();
         }
+
+        static void TestSnoop()
+        {
+            int testWidth = 1920;
+            int testHeight = 1080;
+
+            // Create a bitmap
+            Bitmap bmp = new Bitmap(testWidth, testHeight);
+
+            // Now fill up the Bitmap with some random data
+            // using the bitmap's normal SetPixel() function.
+            Random rand = new Random();
+
+            for (int j = 0; j != bmp.Height; j++)
+            {
+                for (int i = 0; i != bmp.Width; i++)
+                {
+                    bmp.SetPixel(i, j, Color.FromArgb(rand.Next()));
+                }
+            }
+
+            TestGetPixel(bmp);
+            TestSetPixel(bmp);
+            TestGetPixelSpeed(bmp);
+
+
+            int sum = 0;
+
+            /////
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            int height = bmp.Height;
+            int width = bmp.Width;
+
+            for (int j = 0; j != height; j++)
+            {
+                for (int i = 0; i != width; i++)
+                {
+                    sum += bmp.GetPixel(i, j).R;
+                }
+            }
+
+            Trace.WriteLine(string.Format("M Sum: {0}, took: {1}", sum, stopwatch.ElapsedMilliseconds));
+
+
+            var bmpClone = bmp.Clone() as Bitmap;
+            var snoop = new BmpPixelSnoop(bmpClone);
+            /*
+            // Set using normal
+            stopwatch.Reset();
+            stopwatch.Start();
+
+            for (int j = 0; j != height; j++)
+            {
+                for (int i = 0; i != width; i++)
+                {
+                    bmp.SetPixel(i, j, Color.Red);
+                }
+            }
+
+            Trace.WriteLine(string.Format("Set 0, took: {0}", stopwatch.ElapsedMilliseconds));
+            */
+
+            // blaster
+
+            stopwatch.Reset();
+            stopwatch.Start();
+
+ 
+            sum = 0;
+
+            for (int j = 0; j != height; j++)
+            {
+                for (int i = 0; i != width; i++)
+                {
+                    sum += snoop.GetPixel(i, j).R;
+                }
+            }
+
+            Trace.WriteLine(string.Format("B0 Sum: {0}, took: {1}", sum, stopwatch.ElapsedMilliseconds));
+
+            // Using pixel offset
+            // Red channel only
+            stopwatch.Reset();
+            stopwatch.Start();
+
+            sum = 0;
+
+            int size = snoop.Width * snoop.Height;
+            for (int i = 0; i != snoop.Width * snoop.Height; i++)
+            {
+                var col = snoop.GetPixel(i);
+                sum += col.R + col.G + col.B;
+            }
+
+
+            Trace.WriteLine(string.Format("B1 Sum: {0}, took: {1}", sum, stopwatch.ElapsedMilliseconds));
+
+
+
+            // Red channel only
+            stopwatch.Reset();
+            stopwatch.Start();
+
+
+            sum = 0;
+
+
+            for (int j = 0; j != snoop.Height; j++)
+            {
+                for (int i = 0; i != snoop.Width; i++)
+                {
+                    sum += snoop.GetRed(i, j);
+                    sum += snoop.GetGreen(i, j);
+                    sum += snoop.GetBlue(i, j);
+                }
+            }
+
+
+            Trace.WriteLine(string.Format("BR Sum: {0}, took: {1}", sum, stopwatch.ElapsedMilliseconds));
+
+
+            // Red channel only
+            stopwatch.Reset();
+            stopwatch.Start();
+
+
+            sum = 0;
+
+
+            for (int j = 0; j != snoop.Height; j++)
+            {
+                for (int i = 0; i != snoop.Width; i++)
+                {
+                    var col = snoop.NovaGetPixel(i, j);
+                    sum += col.R + col.G + col.B;
+                }
+            }
+
+
+            Trace.WriteLine(string.Format("BN Sum: {0}, took: {1}", sum, stopwatch.ElapsedMilliseconds));
+
+            // Set using blaster
+            stopwatch.Reset();
+            stopwatch.Start();
+
+            sum = 0;
+
+
+            for (int j = 0; j != snoop.Height; j++)
+            {
+                for (int i = 0; i != snoop.Width; i++)
+                {
+                    snoop.SetPixel(i, j, Color.Red);
+                }
+            }
+
+
+            var a = string.Format("Set 1, took: {0}", stopwatch.ElapsedMilliseconds);
+            Trace.WriteLine(a);
+        }
+
+        static void TestGetPixel(Bitmap bmp)
+        {
+            // Deep copy the bitmap
+            var bmpClone = bmp.Clone() as Bitmap;
+
+            // Now iterate over the bitmap clone and compare each pixel
+            // 
+            using (var snoop = new BmpPixelSnoop(bmpClone))
+            { 
+                Console.WriteLine("Testing GetPixel()");
+
+                for (int j = 0; j != bmp.Height; j++)
+                {
+                    for (int i = 0; i != bmp.Width; i++)
+                    {
+                        var p1 = bmp.GetPixel(i, j);
+                        var p2 = snoop.GetPixel(i, j);
+                        if (p1 != p2)
+                        {
+                            Console.WriteLine(string.Format("Pixel at ({0}, {1}) does not match!", i, j));
+                            return;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("GetPixel() OK");
+        }
+
+
+
+        static void TestSetPixel(Bitmap bmp)
+        {
+            // Deep copy the bitmap
+            var bmpClone = bmp.Clone() as Bitmap;
+
+            // Now iterate over the bitmap clone and compare each pixel
+            // 
+            using (var snoop = new BmpPixelSnoop(bmpClone))
+            {
+                Console.WriteLine("Testing SetPixel()");
+
+                for (int j = 0; j != bmp.Height; j++)
+                {
+                    for (int i = 0; i != bmp.Width; i++)
+                    {
+                        var p1 = bmp.GetPixel(i, j);
+                        snoop.SetPixel(i, j, p1);
+                        var p2 = snoop.GetPixel(i, j);
+                        if (p1 != p2)
+                        {
+                            Console.WriteLine(string.Format("Pixel at ({0}, {1}) does not match!", i, j));
+                            return;
+                        }
+                    }
+                }
+
+
+            }
+
+            Console.WriteLine("SetPixel() OK");
+        }
+
+        static void TestGetPixelSpeed(Bitmap bmp)
+        {
+            Console.WriteLine("Testing GetPixel() Speed");
+            // Deep copy the bitmap
+            var bmpClone = bmp.Clone() as Bitmap;
+
+            int height = bmpClone.Height;
+            int width = bmpClone.Width;
+
+            long sum = 0;
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            for (int j = 0; j != height; j++)
+            {
+                for (int i = 0; i != width; i++)
+                {
+                    sum += bmpClone.GetPixel(i, j).R;
+                }
+            }
+
+            var time = stopwatch.ElapsedMilliseconds;
+
+            stopwatch.Reset();
+            stopwatch.Start();
+
+            long snoopSum = 0;
+
+            using (var snoop = new BmpPixelSnoop(bmpClone))
+            {
+                for (int j = 0; j != snoop.Height; j++)
+                {
+                    for (int i = 0; i != snoop.Width; i++)
+                    {
+                        snoopSum += snoop.GetPixel(i, j).R;
+                    }
+                }
+            }
+
+            var snoopTime = stopwatch.ElapsedMilliseconds;
+
+            if (sum != snoopSum)
+            {
+                Console.WriteLine("Pixel sums don't match!");
+                return;
+            }
+
+            Console.WriteLine(string.Format("Bitmap.GetPixel() took {0}ms, BmpPixelSnoop.GetPixel() took {1}ms", time, snoopTime));
+        }
+
     }
 }
